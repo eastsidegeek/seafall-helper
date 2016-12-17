32733d8b8c5f17f3cb8ac9d1306ff8c014e0bae5
Daniel Stafford <daniel.stafford@gmail.com>
2016-12-17 16:48:59 -0600

    Unrolled successes, weaks don't count

--- main/java/com/example/seafalltest/MainActivity.java
+++ main/java/com/example/seafalltest/MainActivity.java
@@ -21,10 +21,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewResults;
     private EditText editTextDiceCount;
     private EditText editTextDefense;
+    private EditText editTextAdditionalSuccesses;
     private String stringDiceCount;
     private String stringDefense;
     private Switch switchRerollWeak;
     private Switch switchDoubleStrong;
+    private Switch switchWeaksDontCount;
     private int intDiceCount;
     private int intDefense;
     private double[] doubleProbs;
@@ -33,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonDefenseDec;
     private Button buttonDefenseInc;
     private Button buttonWizard;
+    private Button buttonAdditionalSuccessesDec;
+    private Button buttonAdditionalSuccessesInc;
 
     private ScrollView scrollView2;
 
@@ -46,18 +50,23 @@ public class MainActivity extends AppCompatActivity {
         buttonCalculate = (Button) findViewById(R.id.buttonCalculate);
         editTextDiceCount = (EditText) findViewById(R.id.editTextDice);
         editTextDefense = (EditText) findViewById(R.id.editTextDefense);
+        editTextAdditionalSuccesses = (EditText) findViewById(R.id.editTextAdditionalSuccesses);
         switchRerollWeak = (Switch) findViewById(R.id.switchRerollWeak);
         switchDoubleStrong = (Switch) findViewById(R.id.switchDoubleStrong);
+        switchWeaksDontCount = (Switch) findViewById(R.id.switchWeaksDontCount);
         buttonDiceDec = (Button) findViewById(R.id.buttonDiceDec);
         buttonDiceInc = (Button) findViewById(R.id.buttonDiceInc);
         buttonDefenseDec = (Button) findViewById(R.id.buttonDefenseDec);
         buttonDefenseInc = (Button) findViewById(R.id.buttonDefenseInc);
         buttonWizard = (Button) findViewById(R.id.buttonWizard);
+        buttonAdditionalSuccessesDec = (Button) findViewById(R.id.buttonAdditionalSuccessesDec);
+        buttonAdditionalSuccessesInc = (Button) findViewById(R.id.buttonAdditionalSuccessesInc);
 
         scrollView2 = (ScrollView) findViewById(R.id.scrollView2);
 
         editTextDiceCount.setInputType(0);
         editTextDefense.setInputType(0);
+        editTextAdditionalSuccesses.setInputType(0);
 
         scrollView2.setOnTouchListener(new View.OnTouchListener() {
            @Override
@@ -68,6 +77,10 @@ public class MainActivity extends AppCompatActivity {
                if (editTextDefense.hasFocus()) {
                    editTextDefense.clearFocus();
                }
+               if(editTextAdditionalSuccesses.hasFocus()) {
+                   editTextAdditionalSuccesses.clearFocus();
+               }
+
                return false;
            }
        });
@@ -194,6 +207,36 @@ public class MainActivity extends AppCompatActivity {
             } // end onClick
         }); // end new OnClickListener (buttonDefenseInc)
 
+        buttonAdditionalSuccessesDec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                stringDiceCount = editTextAdditionalSuccesses.getText().toString();
+                intDiceCount = Integer.parseInt(stringDiceCount);
+                if (intDiceCount == 0) {
+                    return;
+                } else {
+                    intDiceCount--;
+                    stringDiceCount = Integer.toString(intDiceCount);
+                    editTextAdditionalSuccesses.setText(stringDiceCount);
+                }
+            } // end onClick
+        }); // end new OnClickListener (buttonAdditionalSuccessesDec)
+
+        buttonAdditionalSuccessesInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                stringDiceCount = editTextAdditionalSuccesses.getText().toString();
+                intDiceCount = Integer.parseInt(stringDiceCount);
+                if (intDiceCount == 100) {
+                    return;
+                } else {
+                    intDiceCount++;
+                    stringDiceCount = Integer.toString(intDiceCount);
+                    editTextAdditionalSuccesses.setText(stringDiceCount);
+                }
+            } // end onClick
+        }); // end new OnClickListener (buttonAdditionalSuccessesInc)
+
     } // end onCreate
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -214,7 +257,8 @@ public class MainActivity extends AppCompatActivity {
     private double[] findResults ( int intDice){
         double[] localProbs;
         double[] result;
-        int newlength, oldlength;
+        double[] modifiedResult;
+        int newlength, oldlength, iAdditionalSuccesses;
         localProbs = new double[2];
 
         if (switchRerollWeak.isChecked()) {
@@ -229,13 +273,25 @@ public class MainActivity extends AppCompatActivity {
             } // end doubleStrong
         } else {
             if (switchDoubleStrong.isChecked()) {
-                localProbs = new double[3];
-                localProbs[0] = 1.0 / 3.0;
-                localProbs[1] = 1.0 / 2.0;
-                localProbs[2] = 1.0 / 6.0;
+                if (switchWeaksDontCount.isChecked()) {
+                    localProbs = new double[3];
+                    localProbs[0] = 1.0 / 2.0;
+                    localProbs[1] = 1.0 / 3.0;
+                    localProbs[2] = 1.0 / 6.0;
+                } else {
+                    localProbs = new double[3];
+                    localProbs[0] = 1.0 / 3.0;
+                    localProbs[1] = 1.0 / 2.0;
+                    localProbs[2] = 1.0 / 6.0;
+                }
             } else {
-                localProbs[0] = 1.0 / 3.0; // unmodified die roll
-                localProbs[1] = 2.0 / 3.0;
+                if(switchWeaksDontCount.isChecked()) {
+                    localProbs[0] = 1.0 / 2.0;
+                    localProbs[1] = 1.0 / 2.0;
+                } else {
+                    localProbs[0] = 1.0 / 3.0; // unmodified die roll
+                    localProbs[1] = 2.0 / 3.0;
+                }
             } // end doubleStrong
         } // end reroll weak
 
@@ -247,6 +303,20 @@ public class MainActivity extends AppCompatActivity {
         result = new double[newlength];
 
         result = expPolys(localProbs, (intDice - 1));
+
+        iAdditionalSuccesses = Integer.parseInt(editTextAdditionalSuccesses.getText().toString());
+
+        if(iAdditionalSuccesses > 0) {
+            newlength += iAdditionalSuccesses;
+            modifiedResult = new double[newlength];
+            int i;
+            for(i=0; i<iAdditionalSuccesses;i++) {
+                modifiedResult[i] = 0;
+            }
+            System.arraycopy(result, 0, modifiedResult, iAdditionalSuccesses, (newlength-iAdditionalSuccesses));
+
+            return modifiedResult;
+        } // end additionalSuccesses Calculation
 
         return result;
     } // end calculateProbabilityArray
